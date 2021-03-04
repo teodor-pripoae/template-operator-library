abdfef69ca73aa0077fc7494a1b438b3b308a28f
Brendan Galloway <brendan@flanksource.com>
2021-03-04 14:39:43 +0200

    test: Add linting and github workflow

---
+++ test/lint-manifest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+make
+if [ $(git status | grep "working tree clean" | wc -l) -ne "1" ]; then
+  printf "Changes to manifests required, please build and recommit:\n\n"
+  git status
+  git diff
+  exit 1
+fi
